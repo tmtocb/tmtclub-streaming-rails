bb50009f077f52060da510dc1216de2414d4ae1d
tmtocb <kittz.esport@gmail.com>
2021-01-12 15:43:50 +0100

    add validations with tests

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,2 +1,4 @@
 class Organisation < ApplicationRecord
+  validates :name, presence: true
+  validates :slug, presence: true, uniqueness: true
 end
--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Organisation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#validations' do
+
+    it 'should test that factory is valid' do
+      expect(FactoryBot.build :organisation).to be_valid
+    end
+
+    it 'should validate the presence of name' do
+      organisation = FactoryBot.build :organisation, name: ''
+      expect(organisation).not_to be_valid
+      expect(organisation.errors.messages[:name]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of slug' do
+      organisation = FactoryBot.build :organisation, slug: ''
+      expect(organisation).not_to be_valid
+      expect(organisation.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate the uniqueness of slug' do
+      organisation = FactoryBot.create :organisation
+      invalid_organisation = FactoryBot.build :organisation, slug: organisation.slug
+      expect(invalid_organisation).not_to be_valid
+    end
+  end  
 end
